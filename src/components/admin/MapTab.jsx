49588e824494e5c91465338968f40372171e5b8c
Cristian Luke Cabello <cluke@eimontajes.cl>
2026-09-10 10:16:05 -0300

    fix(map): cambiar capa de mapas a OpenStreetMap para evitar requerimiento de API Key de CARTO

--- src/components/admin/MapTab.jsx
+++ src/components/admin/MapTab.jsx
@@ -43,9 +43,9 @@ export default function MapTab({ hookProps }) {
       });
 
       L.tileLayer(
-        "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         {
-          attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
           maxZoom: 19,
         }
       ).addTo(map);
